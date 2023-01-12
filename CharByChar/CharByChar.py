6f705689d7d9f2587b320a773004459208300575
Mechh2 <95701816+Mechh2@users.noreply.github.com>
2023-01-12 20:13:25 +0000

    Added comments

--- CharByChar/CharByChar.py
+++ CharByChar/CharByChar.py
@@ -1,21 +1,22 @@
-from time import sleep
+from time import sleep # This will alow the script to utilise the 'delay' variable
 
-def cs():
+def cs(): # CS stands for ClearScreen
   print("\033c")
 
 class CharByChar:
-    def __init__(self, text="Printing char by char!", delay=0.08333333333):
+    def __init__(self, text="Printing char by char!", delay=0.08333333333): # For best results, use the default delay
         self.letters = []
         self.delay = delay
-        for x in text:
-            self.letters.append(x)
+        for chacter in text:
+            self.letters.append(character)
+        character = None # Clear cached data
         self.line_print()
    
     def line_print(self):
-        to_print = ""
-        for x in self.letters:
-            sleep(self.delay)
+        to_print = "" # To print is essintially the 'text' variable but each character is added after each delay
+        for character in self.letters:
+            sleep(self.delay) # Delay
             cs()
-            to_print += x
+            to_print += character # Character is added after each delay
            
             print(to_print)
